992cd3069f46c11296779aec0378e08a24fef085
Zach Newbery <47321182+ZachNewbery@users.noreply.github.com>
2020-12-11 10:53:40 +0000

    Commit graphing files and assets

---
+++ Assets/Scripts/GraphMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphMenu : MonoBehaviour
+{   
+    public GameObject Panel;
+    // Start is called before the first frame update
+    public void openPanel() {
+        if(Panel != null) {
+            Animator animator = Panel.GetComponent<Animator>();
+
+            if(animator != null) {
+                bool isOpen = animator.GetBool("Open");
+                animator.SetBool("Open", !isOpen);
+            }
+        }
+    }
+}
